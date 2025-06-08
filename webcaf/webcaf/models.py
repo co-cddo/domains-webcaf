061402d509740228336d863d2cee95adb08301a6
Jim Narey <jimnarey@me.com>
2025-06-08 19:22:55 +0100

    Add models
    At this stage, just enough to be able to create a prototype for the
    user's flow through the application.

--- webcaf/webcaf/models.py
+++ webcaf/webcaf/models.py
@@ -1,3 +1,22 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Organisation(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    def __str__(self):
+        return self.name
+
+
+class System(models.Model):
+    name = models.CharField(max_length=255)
+    organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE, related_name='systems')
+    
+    class Meta:
+        unique_together = ['name', 'organisation']
+ 
+
+class UserProfile(models.Model):
+    # Do this rather than add a foreign key to Oranisation, in case we need a many-to-many relationship later
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE, related_name='members', null=True, blank=True)
