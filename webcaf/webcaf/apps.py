f731bb2d61806c8296a4f8e6c7afeb3ce5763042
Jim Narey <jimnarey@me.com>
2025-06-08 23:02:47 +0100

    Add CAF 3.2 router
    With the two field provider classes, the router is the other
    framework-specific piece of code. It can handle versions of the
    CAF which vary in content but not structure and small structural
    changes could probably be handled by subclassing it.
    
    However, if we wanted to accomomodate an assessment framework
    which had a significantly different structure we would need
    a replacement. An immediate next step is to create an ABC from which
    this class can inherit to accomodate this.
    
    The router can create a route with only organisation questions and
    another with only system questions but this is currently unused.
    
    The AppsConfig subclass is used to load the route.
    
    Once the routes are loaded the router is no longer needed.

--- webcaf/webcaf/apps.py
+++ webcaf/webcaf/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
 
 
-class DjangoGovukAppConfig(AppConfig):
+class WebcafConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "webcaf.webcaf"
+    label = "webcaf"
+
+    def ready(self) -> None:
+        from django.conf import settings
+
+        from .caf32_router import FrameworkRouter
+
+        framework_path = getattr(settings, "FRAMEWORK_PATH")
+        self.framework_router = FrameworkRouter(framework_path)
+        self.framework_router.all_route()
