276b4c43ae7eda3d84f40664c1ba0f9d877f9515
Dharshana Vanderbona <dharshana.vanderbona@digital.cabinet-office.gov.uk>
2025-07-23 13:24:01 +0100

    Model updates
    - Mainly the Organisation model update to accommodate the information required in my organisation page.

--- webcaf/webcaf/models.py
+++ webcaf/webcaf/models.py
@@ -3,7 +3,17 @@ from django.db import models
 
 
 class Organisation(models.Model):
+    ORGANISATION_TYPE_CHOICES = [("Government", "Government"), ("Public", "Public")]
     name = models.CharField(max_length=255, unique=True)
+    organisation_type = models.CharField(
+        max_length=255, null=True, blank=True, choices=ORGANISATION_TYPE_CHOICES, default=None
+    )
+    contact_name = models.CharField(max_length=255, null=True, blank=True)
+    contact_email = models.EmailField(null=True, blank=True)
+    contact_role = models.CharField(max_length=255, null=True, blank=True)
+    parent_organisation = models.ForeignKey(
+        "self", on_delete=models.SET_NULL, related_name="sub_organisations", null=True, blank=True
+    )
 
     def __str__(self):
         return self.name
@@ -18,8 +28,13 @@ class System(models.Model):
 
 
 class UserProfile(models.Model):
-    # Do this rather than add a foreign key to Oranisation, in case we need a many-to-many relationship later
+    ROLE_CHOICES = [
+        ("Admin", "Admin"),
+        ("User", "User"),
+    ]
+    # Do this rather than add a foreign key to Organisation, in case we need a many-to-many relationship later
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     organisation = models.ForeignKey(
         Organisation, on_delete=models.CASCADE, related_name="members", null=True, blank=True
     )
+    role = models.CharField(max_length=255, null=True, blank=True, choices=ROLE_CHOICES, default="User")
