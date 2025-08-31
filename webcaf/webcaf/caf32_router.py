6a7ae5d48a62b689ecb31cbd2ca30008a6629fd8
Jim Narey <jimnarey@me.com>
2025-08-31 15:13:19 +0100

    Move ABCs to separate file
    Also rename namedtuple for clarity.

--- webcaf/webcaf/caf32_router.py
+++ webcaf/webcaf/caf32_router.py
@@ -1,5 +1,4 @@
 import logging
-from abc import ABC, abstractmethod
 from typing import Any, Generator
 
 import yaml
@@ -8,6 +7,7 @@ from django.utils.text import slugify
 from django.views.generic import FormView
 
 from webcaf import urls
+from webcaf.webcaf.abcs import FrameworkRouter
 from webcaf.webcaf.views.view_factory import create_form_view
 
 from .caf32_field_providers import (
@@ -22,28 +22,6 @@ FrameworkValue = str | dict | int | None
 FormViewClass = type[FormView]
 
 CAF32Element = dict[str, Any]
-
-
-# This is really just a placeholder for now. Until we add in new frameworks we can't know what makes sense
-# to be included in a common interface.
-class FrameworkRouter(ABC):
-    """
-    This class is the primary interface between the YAML CAF and the rest of the application. It's declared
-    as a class partly in case we later want to use an ABC to declare a common interface for different types
-    of router.
-
-    It reads the YAML and from there can produce a route based on all the outcomes, only those associated with
-    organisations or only those associated with systems. This is done by creating a class for each view and form
-    element in the CAF then updating Django's url patterns with paths to the views. Each form is provided the
-    success_url for the next page in the route.
-    """
-
-    all_url_names: list[str] = []
-    parent_map: dict[str, Any] = {}
-
-    @abstractmethod
-    def execute(self) -> None:
-        pass
 
 
 class CAF32Router(FrameworkRouter):
