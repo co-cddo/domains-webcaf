6a4f3e0e1f4f1c2752b2a7af7964742a2f4187f8
Dharshana Vanderbona <dharshana.vanderbona@digital.cabinet-office.gov.uk>
2025-09-17 15:41:40 +0100

    Add functionality to retrieve color code, status change details, and justification text based on assessment data
    - Created new templatetags `get_tag_for_status`, `get_when_the_status_changed`, `indicator_min_profile_requirement_met`, and `get_justification_text` in `form_extras.py`
    - Added new template partial `principal_summary-list.html` to display principal summaries with status details and change history
    - Implemented new information display on the objective screens
    - Updated migrations to create a new model `HistoricalAssessment` for tracking assessment changes

--- webcaf/webcaf/caf/util.py
+++ webcaf/webcaf/caf/util.py
@@ -1,5 +1,7 @@
 from typing import Any, Dict, Optional
 
+from webcaf.webcaf.models import Assessment
+
 
 class IndicatorStatusChecker:
     @staticmethod
@@ -63,3 +65,136 @@ Please confirm you agree with this status, or you can choose to change the outco
             "override_status": override_status,
             "outcome_status_text": outcome_status_text_map[outcome_status],
         }
+
+    @staticmethod
+    def get_when_the_status_changed(assessment: Assessment, indicator_id: str, status: str) -> Assessment | None:
+        """
+        Provides details about when the status of an indicator within an assessment has changed.
+        Go through the assessment history and find the timestamp of the first occurrence of the given status for the given indicator.
+        :param assessment: Assessment object containing all indicators and their statuses.
+        :param indicator_id: Unique identifier for the indicator whose status change is being tracked.
+        :param status: New status of the indicator after the change.
+
+        :return: Timestamp indicating when the status change occurred.
+        """
+        historical_assessments = assessment.history.all()
+
+        # Filter historical assessments where the indicator's status has changed
+        status_key = IndicatorStatusChecker.status_to_key(status)
+
+        filtered_history = []
+        for i in range(len(historical_assessments) - 1):
+            prev_outcome = (
+                historical_assessments[i]
+                .assessments_data.get(indicator_id, {})
+                .get("confirmation", {})
+                .get("confirm_outcome", "")
+            )
+            next_outcome = (
+                historical_assessments[i + 1]
+                .assessments_data.get(indicator_id, {})
+                .get("confirmation", {})
+                .get("confirm_outcome", "")
+            )
+            # We get the records in reverse order, so we need to check the next outcome first
+            if prev_outcome == f"change_to_{status_key}" and next_outcome != f"change_to_{status_key}":
+                filtered_history.append(historical_assessments[i])
+
+        # If there are no matching statuses, return None
+        if not filtered_history:
+            return None
+
+        # Return the timestamp of the first occurrence
+        return filtered_history[0]
+
+    @classmethod
+    def indicator_min_profile_requirement_met(
+        cls, assessment: Assessment, principal_id: str, indicator_id: str, status: str
+    ) -> str:
+        """
+        Checks if the minimum profile requirement for the given indicator is met.
+        :param assessment:
+        :param principal_id:
+        :param indicator_id:
+        :param status:
+        :return:
+        """
+
+        principal = assessment.get_router().get_section(principal_id[0])["principles"][principal_id]  # type: ignore
+        outcome = principal["outcomes"][indicator_id]
+        min_profile_requirement = outcome.get("min_profile_requirement")
+        profile_scores = {
+            "achieved": 3,
+            "partially_achieved": 2,
+            "not_achieved": 1,
+        }
+        if min_profile_requirement:
+            if status:
+                return (
+                    "Yes"
+                    if profile_scores[cls.status_to_key(status)]
+                    >= profile_scores[cls.status_to_key(min_profile_requirement[assessment.caf_profile])]
+                    else "Not met"
+                )
+            else:
+                return "Not met"
+        else:
+            return "Yes"
+
+    @staticmethod
+    def status_to_key(status: str) -> str:
+        """
+        Converts a status string to its corresponding key.
+
+        :param status: Status in string format. Possible values are "Achieved", "Partially achieved", and "Not achieved".
+        :return: Key for the given status. For example, "Achieved" is converted to "achieved".
+
+        :raises ValueError: If an invalid status value is provided.
+        """
+        if status == "Achieved":
+            status_key = "achieved"
+        elif status == "Partially achieved":
+            status_key = "partially_achieved"
+        elif status == "Not achieved":
+            status_key = "not_achieved"
+        else:
+            raise ValueError(f"Invalid status: {status}")
+        return status_key
+
+    @staticmethod
+    def key_to_status(status: str) -> str:
+        """
+        Converts a string representation of a status into a more formal key.
+
+        :param status:
+        :return:
+        """
+        if status == "achieved":
+            status_key = "Achieved"
+        elif status == "partially_achieved":
+            status_key = "Partially achieved"
+        elif status == "not_achieved":
+            status_key = "Not achieved"
+        else:
+            raise ValueError(f"Invalid key: {status}")
+        return status_key
+
+    @classmethod
+    def get_justification_text(cls, assessments_data: dict[str, Any], indicator_id: str) -> str | None:
+        """
+        Returns the justification text for a given indicator based on assessment data.
+
+        :param assessments_data: Dictionary containing assessment data.
+        :type assessments_data: dict
+
+        :param indicator_id: Identifier of the indicator to retrieve justification text for.
+        :type indicator_id: int
+
+        :return: Justification text if found, otherwise None.
+        :rtype: str | None
+        """
+        confirmation = assessments_data.get(indicator_id, {}).get("confirmation", {})
+        if confirm_outcome := confirmation.get("confirm_outcome"):
+            if confirm_outcome.startswith("change_to_"):
+                return confirmation.get(f"confirm_outcome_{confirm_outcome}_comment")
+        return None
